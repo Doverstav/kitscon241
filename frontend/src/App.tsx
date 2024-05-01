d0373bb2f434a70b2954dfec3a96d75fd3d7e8f3
Pontus Doverstav <doverstav@gmail.com>
2024-05-01 18:35:25 +0200

    Add tabs for different demo areas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { ImageGenerator } from "./components/ImageGenerator";
 import { Chat } from "./components/Chat";
 import { Translator } from "./components/Translator";
 import { Notes } from "./components/Notes";
+import { Tabs, TabList, TabPanel, Tab } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 /* 
 TODO
-- [ ] Tabs for different the different workers
+- [X] Tabs for different the different workers
 - [X] API endpoint that uses some more cloudflare services
 - [X] Clean up code, extract components, hooks etc
 - [ ] Add some styling
@@ -23,10 +25,27 @@ function App() {
         A small demo site using Cloudflare workers as a backend, demonstrating
         simple CR_D with KV as storage and trying out Workers AI.
       </p>
-      <ImageGenerator />
-      <Chat />
-      <Translator />
-      <Notes />
+      <Tabs forceRenderTabPanel>
+        <TabList>
+          <Tab>Image Generator</Tab>
+          <Tab>Chat</Tab>
+          <Tab>Translator</Tab>
+          <Tab>Notes</Tab>
+        </TabList>
+
+        <TabPanel>
+          <ImageGenerator />
+        </TabPanel>
+        <TabPanel>
+          <Chat />
+        </TabPanel>
+        <TabPanel>
+          <Translator />
+        </TabPanel>
+        <TabPanel>
+          <Notes />
+        </TabPanel>
+      </Tabs>
     </>
   );
 }
