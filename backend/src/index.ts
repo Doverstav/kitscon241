5fc5db78127558c122ad077dd6d0494d09f7a6e1
Pontus Doverstav <doverstav@gmail.com>
2024-05-05 11:16:07 +0200

    Add service binding example

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
+import { WorkerEntrypoint } from "cloudflare:workers";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+
+interface DiceRollerService extends WorkerEntrypoint {
+  rollDice: (faces?: number) => Promise<number>;
+}
 
 type Bindings = {
   NOTES: KVNamespace;
   CHAT_HISTORY: KVNamespace;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   AI: any;
+  DICE_ROLLER: Service;
+  DICE_ROLLER_RPC: Service<DiceRollerService>;
 };
 
 interface ImageRequestBody {
@@ -35,8 +42,17 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.use(cors());
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const url = new URL(c.req.url);
+  const requestedFaces = url.searchParams.get("faces");
+  const faces =
+    requestedFaces && !isNaN(parseInt(requestedFaces))
+      ? parseInt(requestedFaces)
+      : 6;
+  const diceVal = await (await c.env.DICE_ROLLER.fetch(url)).text();
+  const diceValRPC = await c.env.DICE_ROLLER_RPC.rollDice(faces);
+
+  return c.text(`${diceVal} & ${diceValRPC}`);
 });
 
 app.post("/api/ai/image", async ({ req, env }) => {
